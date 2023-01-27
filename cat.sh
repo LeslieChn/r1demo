75e421a6b9fa2a3f7b24ead89e7787e0355d8389
Leslie Chang <zz2474@columbia.edu>
2023-01-26 20:36:01 -0500

    catc

--- cat.sh
+++ cat.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
-echo "Meow"
-echo "zzz"
+usage(){
+cat << EE
+	Usage: 
+}
 
+while getopts ":hlp" o; do
+	case "$o" in
+		h)$hflag=1;;
+		l)$lflag=1;;
+		p)$pflag=1;;
+		?)
+			printf "Error: Unknown option '-%s'.\n" "$OPTARG" >&2
+			usage
+			exit 1;
+	esac
+done
